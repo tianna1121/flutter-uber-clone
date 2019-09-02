95e4012455810b6d9dda0c99caf59c28d90bfe00
Fred Jonason <fredjonason@gmail.com>
2019-09-02 11:53:30 +0800

    add _getUserLocation() function to get position data from Google map API

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import './requests/google_maps_requests.dart';
 import 'utils/main.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -24,9 +26,19 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   GoogleMapController mapController;
-  static const _initialPostion = LatLng(12.92, 77.02);
+  GoogleMapsServices _googleMapsServices = GoogleMapsServices();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController destinationColoor = TextEditingController();
+  static LatLng _initialPostion;
   LatLng _lastPostion = _initialPostion;
   final Set<Marker> _markers = {};
+  final Set<Polyline> _polyLines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +53,7 @@ class _MapState extends State<Map> {
             children: <Widget>[
               GoogleMap(
                 initialCameraPosition: CameraPosition(
-                  target: _lastPostion,
+                  target: _initialPostion,
                   zoom: 10.0,
                 ),
                 onMapCreated: onCreated,
@@ -163,6 +175,54 @@ class _MapState extends State<Map> {
                 InfoWindow(title: "Remeber here", snippet: "Good place"),
             icon: BitmapDescriptor.defaultMarker),
       );
+    });
+  }
+
+  List decodePoly(String poly) {
+    var list = poly.codeUnits;
+    var lList = new List();
+    int index = 0;
+    int len = poly.length;
+    int c = 0;
+
+    // * repeating until all attributes are decoded
+    do {
+      var shift = 0;
+      int result = 0;
+
+      // * for decoding value of one attribute
+      do {
+        c = list[index] - 63;
+        result != (c & 0x1F) << (shift * 5);
+        index++;
+        shift++;
+      } while (c >= 32);
+
+      // * if value is negtive then bitwise not the value
+      if (result & 1 == 1) {
+        result = ~result;
+      }
+      var results1 = (result >> 1) * 0.00001;
+      lList.add(results1);
+    } while (index < len);
+
+    // * adding to previous value as done in encoding
+    for (var i = 2; i < lList.length; i++) {
+      lList[i] += lList[i - 2];
+    }
+    print(lList.toString());
+
+    return lList;
+  }
+
+  void _getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemark = await Geolocator()
+        .placemarkFromCoordinates(position.latitude, position.longitude);
+    setState(() {
+      _initialPostion = LatLng(position.latitude, position.longitude);
+      locationController.text = placemark[0].name;
     });
   }
 }
