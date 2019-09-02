e5c81a2b5fa3c35c19f14315f72ddb5e5ece56c7
Fred Jonason <fredjonason@gmail.com>
2019-09-02 11:09:37 +0800

    add picker & destination bar

--- lib/home.dart
+++ lib/home.dart
@@ -51,16 +51,89 @@ class _MapState extends State<Map> {
                 markers: _markers,
                 onCameraMove: _onCameraMove,
               ),
+              // Positioned(
+              //   top: 50.0,
+              //   right: 15.0,
+              //   child: FloatingActionButton(
+              //     onPressed: _onAddMarkerPressed,
+              //     tooltip: "add marker",
+              //     backgroundColor: black,
+              //     child: Icon(
+              //       Icons.add_location,
+              //       color: white,
+              //     ),
+              //   ),
+              //),
               Positioned(
                 top: 50.0,
                 right: 15.0,
-                child: FloatingActionButton(
-                  onPressed: _onAddMarkerPressed,
-                  tooltip: "add marker",
-                  backgroundColor: black,
-                  child: Icon(
-                    Icons.add_location,
-                    color: white,
+                left: 15.0,
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(1.0, 5.0),
+                          blurRadius: 10,
+                          spreadRadius: 3)
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20, top: 5),
+                        width: 10.0,
+                        height: 10.0,
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "Pick up",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 105.0,
+                right: 15.0,
+                left: 15.0,
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(1.0, 5.0),
+                          blurRadius: 10,
+                          spreadRadius: 3)
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20, top: 5),
+                          width: 10.0,
+                          height: 10.0,
+                          child: Icon(
+                            Icons.local_taxi,
+                            color: Colors.black,
+                          ),
+                        ),
+                        hintText: "Destination?",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 16.0)),
                   ),
                 ),
               )
